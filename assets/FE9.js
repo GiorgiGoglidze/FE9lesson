53e247814d1833f783028595efd15e6f738f3fee
GiorgiGoglidze <giorgigoglidze43@gmail.com>
2022-05-23 17:50:10 +0400

    pompt ,  arry

--- assets/FE9.js
+++ assets/FE9.js
@@ -1,12 +1,34 @@
-var str1 = prompt("შეიყვანე თანხა",1000);
-var strprc = prompt ("შეიყვანეთ პროცენტი",10)
+var str1 = prompt("ხელფსი",7500);
+
 
 var sum = parseInt( str1);
-var percent = parseInt (strprc);
 
-var result = sum + sum *percent/100;
+if (sum<=1000 ){
+    alert("ხელფასი დაბალია");
 
-alert ("დაანგარიშების შემდეგ ტანხა იქნება" +result)
+}
+else if ( sum>1000 &&  sum<2000){
+    alert("ხელფასი საშუალოზე დაბალია"); 
+}
+
+else if ( sum>2000 &&  sum<3000){
+    alert("საშუალო ხელფასია"); 
+}
+
+
+else if ( sum>3000 &&  sum<5000){
+    alert("ხელფასი საშუალოზე მაღალია"); 
+}
+
+
+else {
+    alert("მაღალი ხელფასია");
+}
+
+
+
+
+
 
 
 
@@ -20,13 +42,13 @@ var mas =[
 
 var Name;
 var Number;
-var Date;
-var Proper;
+var Age;
+var Being;
 
 
 Name= mas [0] [1];
 Number =mas [1] [3];
-Date =mas [2] [1];
-Proper = mas [3] [2];
+Age =mas [2] [1];
+Being = mas [3] [2];
 
-alert ( Name + Number + Date + Proper)
+alert ( Name +Number + Age + Being)
